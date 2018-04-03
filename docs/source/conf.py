dd9b4036a0194273b739ba87df7fab12c554a390
Ali Pirani <alipirani88@gmail.com>
2018-04-03 12:12:22 -0400

    Update RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,14 @@
 import sys
 import os
 import shlex
+from recommonmark.parser import CommonMarkParser
+#from recommonmark.transform import AutoStructify
+#sys.path.insert(0, os.path.abspath('..'))
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -110,7 +118,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
