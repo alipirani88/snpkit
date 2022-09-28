42e166ad280b61fa1b1e7d66b8c2c98d1b61bc5b
alipirani88 <alipirani88@gmail.com>
2022-09-28 15:52:04 -0400

    Major Code Optimizations for the second part of pipeline

--- config_settings.py
+++ config_settings.py
@@ -4,7 +4,6 @@ __author__ = 'alipirani'
 def ConfigSectionMap(section, Config):
     dict1 = {}
     if not Config.has_section(section):
-        #keep_logging('ERROR: Please Check the section name: \'{}\' in config file'.format(section), 'Please Check the section name: \'{}\' in config file'.format(section), logger, 'exception')
         print "ERROR: Please Check the section name: %s in config file" % section
         exit()
     options = Config.options(section)
