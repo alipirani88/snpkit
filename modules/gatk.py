716ce7201832ead9064edb4c13295362df842cd2
alipirani88 <alipirani88@gmail.com>
2022-09-30 16:50:38 -0400

    Fix Typos

--- modules/gatk.py
+++ modules/gatk.py
@@ -11,9 +11,8 @@ def gatk_filter(final_raw_vcf, out_path, analysis, reference, logger, Config, Av
     if ConfigSectionMap("pipeline", Config)['variant_caller'] == "samtools":
         base_cmd = ConfigSectionMap("gatk", Config)['base_cmd']
         filter_criteria = ConfigSectionMap("SNP_filters", Config)['filter_criteria']
-        print "Using variant filter parameters from: %s" % filter_criteria
         if ConfigSectionMap(filter_criteria, Config)['avg_depth'] == "yes":
-            keep_logging('The average depth filter is turned on.', 'The average depth filter is turned on.', logger, 'info')
+            keep_logging(' - The average depth filter is turned on.', 'The average depth filter is turned on.', logger, 'info')
             low_Dp = float(Avg_dp) / 2
             high_Dp = float(Avg_dp) * 5
             DP_filter = "DP > %s && DP < %s" % (int(low_Dp), int(high_Dp))
@@ -36,7 +35,7 @@ def gatk_filter(final_raw_vcf, out_path, analysis, reference, logger, Config, Av
             call(gatk_filter2_command, logger)
             call(filter_flag_command, logger)
         except sp.CalledProcessError:
-            keep_logging('Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger, 'exception')
+            keep_logging(' - Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger, 'exception')
             sys.exit(1)
         gatk_filter2_final_vcf = "%s/%s_filter2_final.vcf" % (out_path, analysis)
         return gatk_filter2_final_vcf
@@ -45,7 +44,7 @@ def gatk_filter(final_raw_vcf, out_path, analysis, reference, logger, Config, Av
         filter_criteria = ConfigSectionMap("SNP_filters", Config)['filter_criteria']
         keep_logging("Using variant filter parameters from: %s" % filter_criteria, "Using variant filter parameters from: %s" % filter_criteria, logger, 'info')
         if ConfigSectionMap(filter_criteria, Config)['avg_depth'] == "yes":
-            keep_logging('The average depth filter is turned on.', 'The average depth filter is turned on.', logger,
+            keep_logging(' - The average depth filter is turned on.', 'The average depth filter is turned on.', logger,
                          'info')
             low_Dp = float(Avg_dp) / 2
             high_Dp = float(Avg_dp) * 5
@@ -71,7 +70,7 @@ def gatk_filter(final_raw_vcf, out_path, analysis, reference, logger, Config, Av
             call(gatk_filter2_command, logger)
             call(filter_flag_command, logger)
         except sp.CalledProcessError:
-            keep_logging('Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger,
+            keep_logging(' - Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger,
                          'exception')
             sys.exit(1)
         gatk_filter2_final_vcf = "%s/%s_filter2_final.vcf" % (out_path, analysis)
@@ -82,7 +81,7 @@ def gatk_filter_contamination(final_raw_vcf, out_path, analysis, reference, logg
         base_cmd = ConfigSectionMap("gatk", Config)['base_cmd']
         filter_criteria = "contamination_filters"
         if ConfigSectionMap(filter_criteria, Config)['avg_depth'] == "yes":
-            keep_logging('The average depth filter is turned on.', 'The average depth filter is turned on.', logger, 'info')
+            keep_logging(' - The average depth filter is turned on.', 'The average depth filter is turned on.', logger, 'info')
             low_Dp = float(Avg_dp) / 2
             high_Dp = float(Avg_dp) * 5
             DP_filter = "DP > %s && DP < %s" % (int(low_Dp), int(high_Dp))
@@ -104,7 +103,7 @@ def gatk_filter_contamination(final_raw_vcf, out_path, analysis, reference, logg
             call(gatk_filter2_command, logger)
             call(filter_flag_command, logger)
         except sp.CalledProcessError:
-            keep_logging('Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger, 'exception')
+            keep_logging(' - Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger, 'exception')
             sys.exit(1)
         gatk_filter2_final_contamination_vcf = "%s/%s_filter2_final_contamination.vcf" % (out_path, analysis)
         #extract_dp = "egrep -v \"^#\" %s | cut -f 8 | sed 's/^.*DP=\([0-9]*\);.*$/\1/' > %s/%s_depth_values.txt" % (gatk_filter2_final_contamination_vcf, out_path, analysis)
@@ -126,7 +125,7 @@ def gatk_filter_contamination(final_raw_vcf, out_path, analysis, reference, logg
             keep_logging(extract_mq, filter_flag_command, logger, 'debug')
             keep_logging(extract_af, filter_flag_command, logger, 'debug')
         except sp.CalledProcessError:
-            keep_logging('Error in GATK contamination filter step. Exiting.', 'Error in GATK contamination filter step. Exiting.', logger, 'exception')
+            keep_logging(' - Error in GATK contamination filter step. Exiting.', 'Error in GATK contamination filter step. Exiting.', logger, 'exception')
             sys.exit(1)
         header = "pos,af"
         header_cmd = "echo \"%s\" > %s/header.txt" % (header, out_path)
@@ -170,7 +169,7 @@ def gatk_filter_indel(final_raw_vcf, out_path, analysis, reference, logger, Conf
     #         call(gatk_filter2_command, logger)
     #         call(filter_flag_command, logger)
     #     except sp.CalledProcessError:
-    #         keep_logging('Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger, 'exception')
+    #         keep_logging(' - Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger, 'exception')
     #         sys.exit(1)
     #     gatk_filter2_final_vcf = "%s/%s_filter2_indel_final.vcf" % (out_path, analysis)
     #     return gatk_filter2_final_vcf
@@ -179,7 +178,7 @@ def gatk_filter_indel(final_raw_vcf, out_path, analysis, reference, logger, Conf
     filter_criteria = ConfigSectionMap("SNP_filters", Config)['filter_criteria']
     keep_logging("Using variant filter parameters from: %s" % filter_criteria, "Using variant filter parameters from: %s" % filter_criteria, logger, 'info')
     if ConfigSectionMap(filter_criteria, Config)['avg_depth'] == "yes":
-        keep_logging('The average depth filter is turned on.', 'The average depth filter is turned on.', logger,
+        keep_logging(' - The average depth filter is turned on.', 'The average depth filter is turned on.', logger,
                      'info')
         low_Dp = float(Avg_dp) / 2
         high_Dp = float(Avg_dp) * 5
@@ -208,7 +207,7 @@ def gatk_filter_indel(final_raw_vcf, out_path, analysis, reference, logger, Conf
         call(gatk_filter2_command, logger)
         call(filter_flag_command, logger)
     except sp.CalledProcessError:
-        keep_logging('Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger,
+        keep_logging(' - Error in GATK filter step. Exiting.', 'Error in GATK filter step. Exiting.', logger,
                      'exception')
         sys.exit(1)
     gatk_filter2_final_vcf = "%s/%s_filter2_indel_final.vcf" % (out_path, analysis)
@@ -225,10 +224,10 @@ def gatk_DepthOfCoverage(out_sorted_bam, out_path, analysis_name, reference, log
     try:
         call(cmd, logger)
     except sp.CalledProcessError:
-        keep_logging('Error in GATK Depth of Coverage step. Exiting.', 'Error in GATK Depth of Coverage step. Exiting.', logger, 'exception')
+        keep_logging(' - Error in GATK Depth of Coverage step. Exiting.', 'Error in GATK Depth of Coverage step. Exiting.', logger, 'exception')
         sys.exit(1)
     gatk_depth_of_coverage_file = "%s/%s_depth_of_coverage.sample_summary" % (out_path, analysis_name)
-    keep_logging('GATK Depth of Coverage file: {}'.format(gatk_depth_of_coverage_file), 'GATK Depth of Coverage file: {}'.format(gatk_depth_of_coverage_file), logger, 'debug')
+    keep_logging(' - GATK Depth of Coverage file: {}'.format(gatk_depth_of_coverage_file), 'GATK Depth of Coverage file: {}'.format(gatk_depth_of_coverage_file), logger, 'debug')
     return gatk_depth_of_coverage_file
 
 def gatk_vcf2fasta_filter2(only_snp_filter2_vcf_file, out_path, analysis, reference, logger, Config):
@@ -238,21 +237,21 @@ def gatk_vcf2fasta_filter2(only_snp_filter2_vcf_file, out_path, analysis, refere
     try:
         call(vcf2fasta_filter2_cmd, logger)
     except sp.CalledProcessError:
-        keep_logging('Error in GATK vcf 2 fasta step. Exiting.', 'Error in GATK vcf 2 fasta step. Exiting.', logger, 'exception')
+        keep_logging(' - Error in GATK vcf 2 fasta step. Exiting.', 'Error in GATK vcf 2 fasta step. Exiting.', logger, 'exception')
         sys.exit(1)
     if _platform == "darwin":
         change_header_cmd = "sed -i '' 's/>.*/>%s/g' %s_filter2.fasta" % (analysis, only_snp_filter2_vcf_file)
         try:
             call(change_header_cmd, logger)
         except sp.CalledProcessError:
-            keep_logging('Error in sed header change step. Exiting.', 'Error in sed header change step. Exiting.', logger, 'exception')
+            keep_logging(' - Error in sed header change step. Exiting.', 'Error in sed header change step. Exiting.', logger, 'exception')
             sys.exit(1)
     else:
         change_header_cmd = "sed -i 's/>.*/>%s/g' %s_filter2.fasta" % (analysis, only_snp_filter2_vcf_file)
         try:
             call(change_header_cmd, logger)
         except sp.CalledProcessError:
-            keep_logging('Error in sed header change step. Exiting.', 'Error in sed header change step. Exiting.', logger, 'exception')
+            keep_logging(' - Error in sed header change step. Exiting.', 'Error in sed header change step. Exiting.', logger, 'exception')
             sys.exit(1)
     gatk_vcf2fasta_filter2_file = "%s_filter2.fasta" % (only_snp_filter2_vcf_file)
     return gatk_vcf2fasta_filter2_file
@@ -261,7 +260,7 @@ def gatkhaplotypecaller(out_finalbam, out_path, reference, analysis, logger, Con
     base_cmd = ConfigSectionMap("gatk", Config)['base_cmd']
     reference_filename = ConfigSectionMap(reference, Config)['ref_path'] + "/" + ConfigSectionMap(reference, Config)['ref_name']
     cmd = "%s %s -R %s -I %s -O %s/%s_aln_mpileup_raw.vcf" % (base_cmd, ConfigSectionMap("gatk", Config)['haplotype_parameters'], reference_filename, out_finalbam, out_path, analysis)
-    keep_logging('Running Command: [%s]' % cmd, 'Running Command: [%s]' % cmd, logger, 'info')
+    keep_logging(' - Running Command: [%s]' % cmd, 'Running Command: [%s]' % cmd, logger, 'info')
     #os.system(cmd)
     call(cmd, logger)
     final_raw_vcf =  "%s/%s_aln_mpileup_raw.vcf" % (out_path, analysis)
@@ -272,7 +271,7 @@ def gatk_filter1(final_raw_vcf, out_path, analysis, reference):
     base_cmd = ConfigSectionMap("gatk", Config)['base_cmd']
     gatk_filter1_parameter_expression = ConfigSectionMap("gatk")['gatk_filter1_parameter_expression']
     gatk_filter1_command = "%s VariantFiltration -R %s -O %s/%s_filter1_gatk.vcf --variant %s --filter-expression \"%s\" --filter-name PASS_filter1" % (base_cmd, reference, out_path, analysis, final_raw_vcf, gatk_filter1_parameter_expression)
-    keep_logging('Running Command: [%s]' % gatk_filter1_command, 'Running Command: [%s]' % gatk_filter1_command, logger, 'info')
+    keep_logging(' - Running Command: [%s]' % gatk_filter1_command, 'Running Command: [%s]' % gatk_filter1_command, logger, 'info')
     os.system(gatk_filter1_command)
     filter_flag_command = "grep '#\|PASS_filter1' %s/%s_filter1_gatk.vcf > %s/%s_filter1_final.vcf" % (out_path, analysis, out_path, analysis)
     os.system(filter_flag_command)
@@ -297,7 +296,7 @@ def indel_realign(out_marked_sort_bam_rename, reference, out_path, analysis):
 #     base_cmd = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("gatk", Config)['gatk_bin'] + "/" + ConfigSectionMap("gatk", Config)['base_cmd']
 #     reference_filename = ConfigSectionMap(reference)['ref_path'] + "/" + ConfigSectionMap(reference)['ref_name']
 #     cmd = "java -jar %s %s -R %s -I %s -o %s/%s_aln_gatk_raw.vcf" % (base_cmd, ConfigSectionMap("gatk")['haplotype_parameters'], reference_filename, out_finalbam, out_path, analysis)
-#     keep_logging('Running Command: [%s]' % cmd, 'Running Command: [%s]' % cmd, logger, 'info')
+#     keep_logging(' - Running Command: [%s]' % cmd, 'Running Command: [%s]' % cmd, logger, 'info')
 #     os.system(cmd)
 #     final_raw_vcf =  "%s/%s_aln_gatk_raw.vcf" % (out_path, analysis)
 #     return final_raw_vcf
@@ -305,7 +304,7 @@ def indel_realign(out_marked_sort_bam_rename, reference, out_path, analysis):
 def gatk_vcf2fasta_filter1(only_snp_filter1_vcf_file, out_path, analysis, reference):
     base_cmd = ConfigSectionMap("gatk", Config)['base_cmd']
     vcf2fasta_filter1_cmd = "%s FastaAlternateReferenceMaker -R %s -o %s_filter1.fasta --variant %s" % (base_cmd, reference, only_snp_filter1_vcf_file, only_snp_filter1_vcf_file)
-    keep_logging('Running Command: [%s]' % vcf2fasta_filter1_cmd, 'Running Command: [%s]' % vcf2fasta_filter1_cmd, logger, 'info')
+    keep_logging(' - Running Command: [%s]' % vcf2fasta_filter1_cmd, 'Running Command: [%s]' % vcf2fasta_filter1_cmd, logger, 'info')
     os.system(vcf2fasta_filter1_cmd)
     if _platform == "darwin":
         change_header_cmd = "sed -i '' 's/>.*/>%s/g' %s_filter1.fasta" % (analysis, only_snp_filter1_vcf_file)
