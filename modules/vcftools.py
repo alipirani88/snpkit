54e73c6b7cdcb8cc3f0b950e88724213b1fce9b4
alipirani88 <alipirani88@gmail.com>
2022-10-27 16:45:34 -0400

    SNPKIT 2.0 Beta Version.

--- modules/vcftools.py
+++ modules/vcftools.py
@@ -6,15 +6,9 @@ from modules.log_modules import keep_logging
 from modules.logging_subprocess import *
 
 def vcftools_vcf2fasta_filter2(only_snp_filter2_vcf, out_path, analysis, reference, logger, Config):
-    # base_tabix = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['tabix_bin']
-    # base_vcftools_perl = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_perl_bin']
-    # base_vcftools_bin = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_bin']
     bgzip_cmd = "bgzip -f %s" % (only_snp_filter2_vcf)
-    # keep_logging(bgzip_cmd, bgzip_cmd, logger, 'debug')
     tabix_cmd = "tabix %s.gz" % (only_snp_filter2_vcf)
-    # keep_logging(tabix_cmd, tabix_cmd, logger, 'debug')
     vcftools_vcf2fasta_filter2_cmd = "cat %s | vcf-consensus %s.gz > %s_filter2_consensus.fa" % (reference, only_snp_filter2_vcf, only_snp_filter2_vcf)
-    # keep_logging(vcftools_vcf2fasta_filter2_cmd, vcftools_vcf2fasta_filter2_cmd, logger, 'debug')
     try:
         call(bgzip_cmd, logger)
         call(tabix_cmd, logger)
@@ -39,15 +33,9 @@ def vcftools_vcf2fasta_filter2(only_snp_filter2_vcf, out_path, analysis, referen
 
 
 def vcfstats(final_raw_vcf, out_path, analysis, logger, Config):
-    # base_tabix = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['tabix_bin']
-    # base_vcftools_perl = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_perl_bin']
-    # base_vcftools_bin = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_bin']
     bgzip_cmd = "bgzip -f -c %s > %s/%s_aln_mpileup_raw.vcf_5bp_indel_removed.vcf.gz" % (final_raw_vcf, out_path, analysis)
-    # keep_logging(bgzip_cmd, bgzip_cmd, logger, 'debug')
     tabix_cmd = "tabix -f %s.gz" % (final_raw_vcf)
-    # keep_logging(tabix_cmd, tabix_cmd, logger, 'debug')
     vcfstat_cmd = "vcf-stats %s.gz > %s/%s_vcf_stats" % (final_raw_vcf, out_path, analysis)
-    # keep_logging(vcfstat_cmd, vcfstat_cmd, logger, 'debug')
     try:
         call(bgzip_cmd, logger)
         call(tabix_cmd, logger)
@@ -56,7 +44,6 @@ def vcfstats(final_raw_vcf, out_path, analysis, logger, Config):
         keep_logging('- Error in vcftools vcf stats step. Exiting.', '- Error in vcftools vcf stats step. Exiting.', logger, 'exception')
         sys.exit(1)
     vcf_stats_file = "%s/%s_vcf_stats" % (out_path, analysis)
-    # keep_logging('VCF Stats file: {}'.format(vcf_stats_file), 'VCF Stats file: {}'.format(vcf_stats_file), logger, 'debug')
     return vcf_stats_file
 
 def only_snp_filter2_vcf(gatk_filter2_final_vcf, out_path, analysis, reference):
@@ -67,9 +54,6 @@ def only_snp_filter2_vcf(gatk_filter2_final_vcf, out_path, analysis, reference):
     return only_snp_filter2_vcf_file
 
 def only_snp_filter1_vcf(gatk_filter1_final_vcf, out_path, analysis, reference):
-    # base_tabix = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['tabix_bin']
-    # base_vcftools_perl = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_perl_bin']
-    # base_vcftools_bin = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_bin']
     onlysnp_filter1_cmd = "vcftools --vcf %s --remove-indels --recode --recode-INFO-all --out %s/%s_filter1_onlysnp.vcf" % (gatk_filter1_final_vcf, out_path, analysis)
     keep_logging('Running Command: [%s]' % onlysnp_filter1_cmd, 'Running Command: [%s]' % onlysnp_filter1_cmd, logger, 'info')
     keep_logging(onlysnp_filter1_cmd, onlysnp_filter1_cmd, logger, 'debug')
@@ -77,18 +61,12 @@ def only_snp_filter1_vcf(gatk_filter1_final_vcf, out_path, analysis, reference):
     return only_snp_filter1_vcf_file
 
 def only_snp_raw_vcf(final_raw_vcf, out_path, analysis, reference):
-    # base_tabix = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['tabix_bin']
-    # base_vcftools_perl = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_perl_bin']
-    # base_vcftools_bin = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_bin']
     onlysnp_raw_cmd = "vcftools --vcf %s --remove-indels --recode --recode-INFO-all --out %s/%s_raw_onlysnp.vcf" % (final_raw_vcf, out_path, analysis)
     keep_logging(onlysnp_raw_cmd, onlysnp_raw_cmd, logger, 'debug')
     only_snp_raw_vcf_file = "%s/%s_raw_onlysnp.vcf.recode.vcf" % (out_path, analysis)
     return only_snp_raw_vcf_file
 
 def vcftools_vcf2fasta_filter1(only_snp_filter1_vcf_file, out_path, analysis, reference):
-    # base_tabix = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['tabix_bin']
-    # base_vcftools_perl = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_perl_bin']
-    # base_vcftools_bin = ConfigSectionMap("bin_path", Config)['binbase'] + "/" + ConfigSectionMap("vcftools", Config)['vcftools_bin']
     bgzip_cmd = "bgzip -f %s" % (only_snp_filter1_vcf_file)
     keep_logging(bgzip_cmd, bgzip_cmd, logger, 'debug')
     try:
